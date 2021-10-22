07f49a55d901ca60fd67775fc299594b2d966330
John Mark Reyes <john@moneyme.com.au>
2021-10-22 12:17:47 +0800

    implement new woocommerce journey

--- includes/functions.php
+++ includes/functions.php
@@ -28,6 +28,7 @@ if ( ! function_exists( 'is_woocommerce_activated' ) ) {
     //63009 = declined
     //63008 = approved
     global $woocommerce;
+    $woocommerce->session->set('chosen_payment_method', 'mme_gateway');
     $checkout_url = sanitize_text_field($_GET['checkout_url']);
     $order_id = sanitize_text_field($_GET['order_id']);
     if(!$order_id){
@@ -91,6 +92,47 @@ if ( ! function_exists( 'is_woocommerce_activated' ) ) {
        if($response->StatusId == 63006){
           $order->update_status('cancelled');
           wc_add_notice( __( 'Your Payment is Declined.', 'gateway' ), 'error' );
+          wp_safe_redirect( wc_get_page_permalink( 'checkout' ) );
+          exit;
+       }elseif($response->StatusId == 63011){
+            $woocommerce->cart->empty_cart();
+            $data = $order->data;
+            $billing = $order->data['billing'];
+            $firstname = $billing['first_name'];
+            $lastname = $billing['last_name'];
+            $company = $billing['company'];
+            $address_1 = $billing['address_1'];
+            $address_2 = $billing['address_2'];
+            $city = $billing['city'];
+            $state = $billing['state'];
+            $postcode = $billing['postcode'];
+            $country = $billing['country'];
+            $email = $billing['email'];
+            $phone = $billing['phone'];
+            
+            $woocommerce->customer->set_billing_first_name($firstname);
+            $woocommerce->customer->set_billing_last_name($lastname);
+            $woocommerce->customer->set_billing_company($company);
+            $woocommerce->customer->set_billing_address_1($address_1);
+            $woocommerce->customer->set_billing_address_2($address_2);
+            $woocommerce->customer->set_billing_city($city);
+            $woocommerce->customer->set_billing_state($state);
+            $woocommerce->customer->set_billing_postcode($postcode);
+            $woocommerce->customer->set_billing_country($country);
+            $woocommerce->customer->set_billing_email($email);
+            $woocommerce->customer->set_billing_phone($phone);
+            foreach ( $order->get_items() as $item_id => $item ) {
+                $product_id = $item->get_product_id();
+                $variation_id = $item->get_variation_id();
+                $quantity = $item->get_quantity();
+                if ($variation_id){
+                  $product_id = $variation_id;
+                }
+                $woocommerce->cart->add_to_cart( $product_id, $quantity);
+            }
+          $order->update_status('cancelled');
+          $order->add_order_note( 'Customer has successfully signed for an MME+ account and was redirected back to your website to complete a new order.');
+          wc_add_notice( __( 'Awesome! Your MoneyMe Plus account has been approved. Please reselect MoneyMe Plus as the payment option and login to complete your purchase.', 'gateway' ), 'notice' );
           wp_safe_redirect( wc_get_page_permalink( 'checkout' ) );
           exit;
        }else{
